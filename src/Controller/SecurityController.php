e4c18a94eae84073c34a0d241ee5839a3da767f5
Maciej Pilarczyk <m.pilarczyk@outlook.com>
2019-07-09 15:03:56 +0200

    Login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,56 +10,72 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
 
-    public function login(
-        Request $request,
-        UserPasswordEncoderInterface $passwordEncoder,
-        GuardAuthenticatorHandler $guardHandler,
-        LoginFormAuthenticator $authenticator
-    ): Response {
-        $user = new User();
-
-        $form = $this->createForm(LoginFormType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-//            // encode the plain password
-//            $user->setPassword(
-//                $passwordEncoder->encodePassword(
-//                    $user,
-//                    $form->get('plainPassword')->getData()
-//                )
-//            );
-//
-//            $entityManager = $this->getDoctrine()->getManager();
-//            $entityManager->persist($user);
-//            $entityManager->flush();
-//
-//            // do anything else you need here, like send an email
-//
-//            return $guardHandler->authenticateUserAndHandleSuccess(
-//                $user,
-//                $request,
-//                $authenticator,
-//                'main' // firewall name in security.yaml
-//            );
-        }
+    public function login(Request $request, AuthenticationUtils $authenticationUtils)
+    {
+        $form = $this->createForm(LoginFormType::class);
 
         return $this->render('security/login.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
 
-//        // get the login error if there is one
-//        $error = $authenticationUtils->getLastAuthenticationError();
-//        // last username entered by the user
-//        $lastUsername = $authenticationUtils->getLastUsername();
+
+    //<form action="{{ path('form_login_check') }}" method="post">
+
+//    public function login(
+//        Request $request,
+//        UserPasswordEncoderInterface $passwordEncoder,
+//        GuardAuthenticatorHandler $guardHandler,
+//        LoginFormAuthenticator $authenticator
+//    ): Response {
+//        $form = $this->createForm(LoginFormType::class);
+//        $form->handleRequest($request);
+//
+//        if ($form->isSubmitted() && $form->isValid()) {
+//
+//
+//
+////            dump($form->getData());exit;
+//
+////            // encode the plain password
+////            $user->setPassword(
+//                $passwordEncoder->encodePassword(
+//                    $user,
+//                    $form->get('plainPassword')->getData()
+//                )
+////            );
+////
+////            $entityManager = $this->getDoctrine()->getManager();
+////            $entityManager->persist($user);
+////            $entityManager->flush();
+////
+////            // do anything else you need here, like send an email
+////
+////            return $guardHandler->authenticateUserAndHandleSuccess(
+////                $user,
+////                $request,
+////                $authenticator,
+////                'main' // firewall name in security.yaml
+////            );
+//        }
 //
 //        return $this->render('security/login.html.twig', [
-//            'last_username' => $lastUsername,
-//            'error' => $error,
+//            'form' => $form->createView(),
 //        ]);
-    }
+//
+////        // get the login error if there is one
+////        $error = $authenticationUtils->getLastAuthenticationError();
+////        // last username entered by the user
+////        $lastUsername = $authenticationUtils->getLastUsername();
+////
+////        return $this->render('security/login.html.twig', [
+////            'last_username' => $lastUsername,
+////            'error' => $error,
+////        ]);
+//    }
 }
--- src/Form/LoginFormType.php
+++ src/Form/LoginFormType.php
@@ -2,21 +2,29 @@
 
 namespace Adshares\Adclassify\Form;
 
-use Adshares\Adclassify\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Core\Security;
 
 class LoginFormType extends AbstractType
 {
+    private $requestStack;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email', EmailType::class, [
+            ->add('username', EmailType::class, [
                 'attr' => [
                     'placeholder' => 'Email address'
                 ],
@@ -25,15 +33,33 @@ class LoginFormType extends AbstractType
                 'attr' => [
                     'placeholder' => 'Password'
                 ],
-                'mapped' => false,
-            ])
-        ;
+            ]);
+
+        $request = $this->requestStack->getCurrentRequest();
+
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) use ($request) {
+            dump($request);exit;
+            if ($request->attributes->has(Security::AUTHENTICATION_ERROR)) {
+                $error = $request->attributes->get(Security::AUTHENTICATION_ERROR);
+            } else {
+                $error = $request->getSession()->get(Security::AUTHENTICATION_ERROR);
+            }
+            if ($error) {
+                $event->getForm()->addError(new FormError($error->getMessage()));
+            }
+            $event->setData(array_replace((array)$event->getData(), array(
+                'username' => $request->getSession()->get(Security::LAST_USERNAME),
+            )));
+        });
     }
 
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults([
-            'data_class' => User::class,
-        ]);
-    }
+//    public function configureOptions(OptionsResolver $resolver)
+//    {
+//        /* Note: the form's csrf_token_id must correspond to that for the form login
+//         * listener in order for the CSRF token to validate successfully.
+//         */
+//        $resolver->setDefaults([
+//            'csrf_token_id' => 'authenticate',
+//        ]);
+//    }
 }
