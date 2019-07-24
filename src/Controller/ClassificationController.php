c670f2577ff740d033b2c7dad4f4f2954e956856
Maciej Pilarczyk <m.pilarczyk@outlook.com>
2019-07-24 19:32:27 +0200

    Pagination

--- src/Controller/ClassificationController.php
+++ src/Controller/ClassificationController.php
@@ -5,6 +5,7 @@ namespace Adshares\Adclassify\Controller;
 use Adshares\Adclassify\Repository\ClassificationRepository;
 use Adshares\Adclassify\Repository\RequestRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class ClassificationController extends AbstractController
@@ -33,13 +34,19 @@ class ClassificationController extends AbstractController
         return $this->render('classification/index.html.twig', []);
     }
 
-    public function status(): Response
+    public function status(Request $request): Response
     {
+        $limit = 100;
+        $page = max(1, (int)$request->query->get('page', 1));
+        $sort = 'id';
+        $order = 'desc';
 
-        $requests = $this->requestRepository->findBy([], ['id' => 'DESC'], 20, 0);
+        $requests = $this->requestRepository->findPaginated($limit, ($page - 1) * $limit, $sort, $order);
 
         return $this->render('classification/status.html.twig', [
             'requests' => $requests,
+            'currentPage' => $page,
+            'totalPages' => ceil($requests->count() / $limit),
         ]);
     }
 }
--- src/Repository/RequestRepository.php
+++ src/Repository/RequestRepository.php
@@ -6,12 +6,30 @@ use Adshares\Adclassify\Entity\Request;
 use Adshares\Adclassify\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 
 class RequestRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Request::class);
+    }
+
+    public function findPaginated(
+        int $limit = 2,
+        int $offset = 0,
+        ?string $sort = null,
+        ?string $order = null
+    ): Paginator {
+        $query = $this->createQueryBuilder('e')
+            ->setFirstResult($offset)
+            ->setMaxResults($limit);
+
+        if ($sort !== null) {
+            $query->orderBy('e.' . $sort, $order);
+        }
+
+        return new Paginator($query, $fetchJoinCollection = false);
     }
 
     public function findPending(): array
